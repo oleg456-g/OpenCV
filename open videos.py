2e59d84be6fb101fc91862577d9abb80088b64ff
oleg456-g <62814783+oleg456-g@users.noreply.github.com>
2022-05-15 11:04:16 +0300

    Update open videos.py

--- open videos.py
+++ open videos.py
@@ -1,6 +1,6 @@
 import cv2
 cap = cv2.VideoCapture(r'videos\New Year - 100050.mp4')
-#
+# Чтение по кадрам видео и его вывод
 while True:
     success, img = cap.read()
     cv2.imshow('Result', img)
